68187fa97c55061881e0e4a3390f7b34c14392a9
John Doe <johndoe@email.com>
2025-01-15 10:46:38 -0600

    course

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #This is my first program 
 print('Hello world')
 print("It's really good!")
+
+# Variable = It is an container for a value (string, integer, float, boolean)
+# A variable behaves as if it was the value it contains. 
+
+first_name = "Hariswar"
+print(first_name)
+print(f"Hello {first_name}") # f - means format 
+#Output would be Hello Hariswar 
+
