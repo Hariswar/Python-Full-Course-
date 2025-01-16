2fe941e37b8a68b7b5f06e13b23ac99d44936de1
John Doe <johndoe@email.com>
2025-01-16 14:37:41 -0600

    course

--- lesson_two.py
+++ lesson_two.py
@@ -1,2 +1,9 @@
 #Typecasting = the process of converting a variable from one data type to another 
 # str(), int(), float(), bool()
+
+name = "Bro Code"
+age = 25
+gpa = 4.0
+is_student = True 
+
+print(type(name)) # this outputs the what data type is each variable. 
