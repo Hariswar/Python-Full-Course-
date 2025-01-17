8726c5a8151d4ca1bb589b99eedb59e98403d26b
John Doe <johndoe@email.com>
2025-01-16 18:51:00 -0600

    course

--- lesson_two.py
+++ lesson_two.py
@@ -15,3 +15,20 @@ print(gpa)
 #int to float
 age = float(age)
 print(age)
+
+#converts to string
+age = str(age)
+print(age)
+print(type(age)) #shows the data type. 
+
+#converts string to bool
+#Example 1
+name = bool(name)
+print(name) #Output would be true 
+
+#Example 2 
+#In this case output would be false since there is no characters inside the string. 
+fruit = ""
+fruit = bool(fruit)
+print(fruit) 
+
