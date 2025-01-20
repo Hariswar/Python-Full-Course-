b09fefb7564d8a43928185f9ac6f25084f92da6b
John Doe <johndoe@email.com>
2025-01-19 18:03:57 -0600

    course

--- lesson_three.py
+++ lesson_three.py
@@ -1,5 +1,9 @@
 #input() = A function that prompts the user to enter data 
 #          Returns the entered data as a string 
 
-name = input("What is ur name?: ")
+name = input("What is ur name?: ") #Asks for your name
 print(f"Hello {name}!")
+
+age = input("How old are you?: ")
+age = int(age) #Turns the string into an int
+print(f"You are {age} years old")
