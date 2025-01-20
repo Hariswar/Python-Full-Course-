b4c3f69040a344f4094189d26e790ef66bcd134b
John Doe <johndoe@email.com>
2025-01-20 16:18:23 -0600

    course

---
+++ Exercise_one.py
@@ -0,0 +1,7 @@
+#Exercise 1 Rectage Area Calc
+
+length = int(input(f"Please type in the length of the Rectangle: "))
+width = int(input(f"Please type in the width of the Rectangle: "))
+area = length * width
+
+print(area)
--- lesson_three.py
+++ lesson_three.py
@@ -4,6 +4,15 @@
 name = input("What is ur name?: ") #Asks for your name
 print(f"Hello {name}!")
 
+#Example 1
+
 age = input("How old are you?: ")
 age = int(age) #Turns the string into an int
+age = age + 1
 print(f"You are {age} years old")
+
+#OR 
+ages = int(input("How old are you?: "))
+ages = ages + 1
+print(f"You are {ages} years old")
+
