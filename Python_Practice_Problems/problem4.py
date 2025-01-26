e3992a150f52b74c0c9445f5daf4742b08ee5232
John Doe <johndoe@email.com>
2025-01-26 15:58:26 -0600

    practice problems

---
+++ Python_Practice_Problems/problem4.py
@@ -0,0 +1,8 @@
+# 4. Write a program that will give you the sum of 3 digits
+
+num1 = int(input("Provide the value to add: "))
+num2 = int(input("Provide the value to add: "))
+num3 = int(input("Provide the value to add: "))
+
+value = num1 + num2 + num3
+print(f"The sum of these three numbers is {value}")
