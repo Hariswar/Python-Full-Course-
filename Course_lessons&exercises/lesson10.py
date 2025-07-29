1896dec96a38d65ae95e3fc1383be4a77fd4b351
Hariswar <bkhariswar@gmail.com>
2025-07-29 17:03:33 -0500

    course

--- Course_lessons&exercises/lesson10.py
+++ Course_lessons&exercises/lesson10.py
@@ -1,3 +1,6 @@
 # indexing = accessing elements of a sequence using [] (indexing operators)
 #         [start : end : step]
 
+credit_number = "1234 5678 9012 3456"
+print(credit_number[0])  # Output: '1'
+print(credit_number[0:4])  # Output: '1234'
