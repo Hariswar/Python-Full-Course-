fdd57346fbb9f8bf0ce671472f7ae60c48da45e6
Hariswar <bkhariswar@gmail.com>
2025-07-30 09:41:36 -0500

    course

--- Course_lessons&exercises/lesson10.py
+++ Course_lessons&exercises/lesson10.py
@@ -1,6 +1,20 @@
 # indexing = accessing elements of a sequence using [] (indexing operators)
 #         [start : end : step]
 
-credit_number = "1234 5678 9012 3456"
+#lesson 1
+
+credit_number = "1234-5678-9012-3456"
+
 print(credit_number[0])  # Output: '1'
 print(credit_number[0:4])  # Output: '1234'
+print(credit_number[5:9])  # Output: '5678'
+print(credit_number[5:]) # Output: '5678-9012-3456'
+print(credit_number[-1])  # Output: '6'
+print(credit_number[::2 ])  # Output: '135790246'
+
+#lesson 2
+last_digits = credit_number[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")  # Output: 'XXXX-XXXX-XXXX-3456'
+
+credit_number = credit_number[::-1]
+print(credit_number)  # Output: '6543-2109-8765-4321'
--- Python_Practice_Problems/problem5.py
+++
@@ -1,1 +0,0 @@
-# 5. Write a program that will reverse a four digit number. Also it checks whether the reverse is true.
