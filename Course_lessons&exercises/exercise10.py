4c265d0dfd9a903d73d40328b32555de2eacb7cc
Hariswar <bkhariswar@gmail.com>
2025-08-02 09:27:13 -0500

    course

--- Course_lessons&exercises/exercise10.py
+++ Course_lessons&exercises/exercise10.py
@@ -19,3 +19,11 @@ price3 = 12.34
 print(f"Price 1 is ${price1:.2f}") # output: Price 1 is $3.14
 print(f"Price 2 is ${price2:10}") # output: Price 2 is $   -987.65
 print(f"Price 3 is ${price3:010}") # output: Price 3 is $0000012.34
+print(f"Price 1 is ${price1:>10}") # output: Price 1 is $      3.14
+print(f"Price 2 is ${price2:^10}") # output: Price 2 is $  -987.65
+
+price4 = 3000.14159
+
+print(f"Price 4 is ${price4:+,.2f}") # output: Price 4 is $+3,000.14
+
+
