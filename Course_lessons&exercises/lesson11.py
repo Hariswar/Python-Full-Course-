fbae1cbd0721d4c791bfa1a2faab6857832967ec
Hariswar <bkhariswar@gmail.com>
2025-08-03 09:17:52 -0500

    course

--- Course_lessons&exercises/lesson11.py
+++ Course_lessons&exercises/lesson11.py
@@ -3,11 +3,20 @@
 name = input("Enter your name: ")
 
 while name == "":
-  print("You did not enter your name")
-  name = input("Please enter your name: ")
+  print("You did not enter your name") 
+  name = input("Please enter your name: ") # Prompt the user again
 else:
   print(f"Hello {name}!")
-  
+
+#lesson 2: 
+
+age = int(input("Enter your age: "))
+
+while age < 0:
+  print("Age can't be negative")
+  age = int(input("Enter your age: "))
+
+print(f"You are {age} years old!")
 
 
 
