96df58ff3a17b83a3be7dea6a3d1c108542ff9a2
Hariswar <bkhariswar@gmail.com>
2025-08-12 10:42:05 -0500

    course

--- Course_lessons&exercises/lesson12.py
+++ Course_lessons&exercises/lesson12.py
@@ -1,5 +1,14 @@
 #for loops = execute a block of code a fixed number of times. 
 #            You can iterate over a range, string, sequence, etc. 
 
+# Example 1: 
 for x in range(1, 11): # 1 - 10
+  print(x) 
+  # Output would be 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+# Example 2:
+for x in reversed(range(1,11)):
   print(x)
+print("Happy new year!")
+# Output would be 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 Happy new year!
+
